13032b350db30a3f6041c42cde099ef55b4af9ab
Manoj Singh <manojsinghrana467@gmail.com>
2021-12-23 18:34:25 +0530

    Added images to the products

--- src/CartItem.js
+++ src/CartItem.js
@@ -7,7 +7,7 @@ const CartItem = (props) => {
   return (
     <div className="cart-item">
       <div className="left-block">
-        <img style={styles.image} />
+        <img style={styles.image} src={product.img} />
       </div>
       <div className="right-block">
         <div style={{ fontSize: 25 }}>{title}</div>
