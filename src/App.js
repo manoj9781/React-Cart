b66e4d3a0a7ded04ff28deddc7aad218db7b32bd
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-03 14:31:48 +0530

    Remove local states

--- src/App.js
+++ src/App.js
@@ -10,29 +10,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Iphone',
-          qty: 2,
-          img: 'https://images.unsplash.com/photo-1537589376225-5405c60a5bd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aXBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
-          id: 1,
-        },
-        {
-          price: 90,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
-          id: 2,
-        },
-        {
-          price: 990,
-          title: 'Laptop',
-          qty: 3,
-          img: 'https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
-          id: 3,
-        },
-      ],
+      products: [],
+      loading: true,
     };
   }
 
