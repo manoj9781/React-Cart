35eb6a5b0625e4e30fd0439890cf05436e18f3f6
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-03 12:54:45 +0530

    Get Data from database

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Navbar from './Navbar';
 import './index.css';
 
 // import * as firebase from 'firebase';
-import firebase from 'firebase'
-
-
-
+import firebase from 'firebase';
 
 class App extends React.Component {
   constructor() {
@@ -40,11 +37,22 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // firebase
+    //   .firestore()
+    //   .collection('products')
+    //   .get()
+    //   .then((snapshot) => {
+    //     const products = snapshot.docs.map((doc) => {
+    //       const data = doc.data();
+    //       data['id'] = doc.id;
+    //       return data;
+    //     });
+    //     this.setState({ products: products, loading: false });
+    //   });
     firebase
       .firestore()
       .collection('products')
-      .get()
-      .then((snapshot) => {
+      .onSnapshot((snapshot) => {
         const products = snapshot.docs.map((doc) => {
           const data = doc.data();
           data['id'] = doc.id;
